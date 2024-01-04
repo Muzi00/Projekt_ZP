329e3bf47765ef8f6dc2c1ca24bc3693359be7d8
Adam <adamos139@wp.pl>
2024-01-04 16:44:32 +0100

    rozdzielenie pobranych danych, wyszukiwanie w kilku województwach

--- mainFastAPI.py
+++ mainFastAPI.py
@@ -19,31 +19,41 @@ async def get_data(asin: str):
         session.headers.update({
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
         })
-        resp = session.get(f"https://www.otodom.pl/pl/wyniki/sprzedaz/mieszkanie/mazowieckie/{asin}")
-
-        if resp.status_code != 200:
-            logging.error(f"Bad status code: {resp.status_code}")
-            return {"error": f"Bad status code {resp.status_code}"}
-
-        soup = BeautifulSoup(resp.text, "html.parser")
-
-        price_elements = soup.select("[class='e1jyrtvq0 css-1tjkj49 ei6hyam0']")
-
         results = []
+#        for woj in ["mazowieckie","slaskie","dolnoslaskie","kujawsko--pomorskie",
+#"lodzkie","lubelskie","lubuskie","malopolskie","opolskie","podkarpackie",
+#"podlaskie","pomorskie","swietokrzyskie","warminsko--mazurskie","wielkopolskie",
+#"zachodniopomorskie"]:
+        for woj in ["mazowieckie"]:
+            resp = session.get(f"https://www.otodom.pl/pl/wyniki/sprzedaz/mieszkanie/"+woj+"/{asin}")
 
-        for price_element in price_elements:
-            cena = price_element.text.strip()
-            miasto = asin
+            if resp.status_code != 200:
+                logging.error(f"Bad status code: {resp.status_code}")
+                return {"error": f"Bad status code {resp.status_code}"}
 
-            data = {
-                "miasto": miasto,
-                "cena": cena,
-            }
+            soup = BeautifulSoup(resp.text, "html.parser")
+
+            price_elements = soup.select("[class='e1jyrtvq0 css-1tjkj49 ei6hyam0']")
+
+            #results = []
+
+            for price_element in price_elements:
+                cena = price_element.text.replace("zł","!").replace("/m²","").replace(" pokoje","!").replace(" pokój","!").replace(" pokojów","!").replace("m²","!").split("!")
+                miasto = asin
+
+                data = {
+                    "województwo": woj,
+                    "miasto": miasto,
+                    "cena": cena[0]+"zł",
+                    "cena /m2": cena[1]+"zł/m²",
+                    "pokoje": cena[2],
+                    "metraż": cena[3]+"m²",
+                }
 
 
 
 
-            results.append(data)
+                results.append(data)
 
 
         return {"results": results}
